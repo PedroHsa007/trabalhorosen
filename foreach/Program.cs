31d375492720af507fc24ccd94af350aa591a4d5
202120423 <202120423@TPCP04LAB1010>
2023-09-27 18:38:25 -0300

    Adicionar arquivos de projeto.

---
+++ foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿var fibNumbers = new List<int> { 0, 1, 1, 2, 3, 5, 8, 13 };
+foreach (int element in fibNumbers)
+{
+    Console.Write($"{element} ");
+}
+
+
+// Span<int> numbers = new int[] { 3, 14, 15, 92, 6 };
+//foreach (int number in numbers)
+//{
+//Console.Write($"{number} ");
+//}
+
+var nomes = new List<string>() { "Jose", "Tania", "Pedro" };
+foreach(string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
